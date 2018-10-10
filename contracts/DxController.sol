e8b04081acab2fcbdf338630db9d0b816653cd2d
Angel Rodriguez <anxolin@gmail.com>
2018-10-10 20:28:53 +0200

    Add networks extraction

--- contracts/DxController.sol
+++ contracts/DxController.sol
@@ -1,13 +1,8 @@
 pragma solidity ^0.4.24;
 
 import "@daostack/arc/contracts/controller/Controller.sol";
-// import "@daostack/arc/contracts/controller/Avatar.sol";
-// import "./Controller2.sol";
 
 contract DxController is Controller {
-    // constructor (Avatar _avatar)
-    // Controller(_avatar) public {}
-
     constructor (Avatar _avatar)
     Controller(_avatar) public {}
 }
