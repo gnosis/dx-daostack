8fc1e802e12920a1427121900cc7a1366cf34905
Velenir <velenir@gmail.com>
2018-11-12 15:54:34 +0300

    fix LOCK_PERIOD timing (was in the past)

--- src/config/timePeriods/initialLocking.js
+++ src/config/timePeriods/initialLocking.js
@@ -2,8 +2,27 @@ const dateUtil = require('../../helpers/dateUtil')
 
 // The lock peridod has some default config, but it ca be modified with
 // env variables
-const LOCK_PERIOD_START = '2018-11-01 00:00'
-const LOCK_PERIOD_END = '2019-01-31 23:59:59'
+const now = new Date()
+
+now.setUTCMonth(now.getUTCMonth() + 1)
+now.setUTCDate(1)
+const YY1 = now.getUTCFullYear()
+const MM1 = now.getUTCMonth()
+const DD1 = now.getUTCDate()
+// 1st of next month
+const LOCK_PERIOD_START = `${YY1}-${MM1 + 1}-${DD1} 00:00`
+
+now.setUTCMonth(MM1 + 3)
+now.setUTCDate(0)
+const YY2 = now.getUTCFullYear()
+const MM2 = now.getUTCMonth()
+const DD2 = now.getUTCDate()
+// last day of the third month from now
+const LOCK_PERIOD_END = `${YY2}-${MM2 + 1}-${DD2} 23:59:59`
+
+// e.g. for 2018-11-any
+// LOCK_PERIOD_START = '2018-12-01 00:00'
+// LOCK_PERIOD_END = '2019-02-28 23:59:59'
 
 module.exports = {
   startDate: dateUtil.parse(
