e4edccd61ad37119b086c1cf611c6ff4b0346291
Velenir <velenir@gmail.com>
2018-11-13 18:46:08 +0300

    add Auction4Reputation-derived contract

---
+++ contracts/DxAuction4Rep.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.24;
+
+import "@daostack/arc/contracts/schemes/Auction4Reputation.sol";
+
+/**
+ * @title Scheme that allows to get GEN by locking MGN
+ */
+contract DxAuction4Rep is Auction4Reputation {
+    constructor () public {}
+}
