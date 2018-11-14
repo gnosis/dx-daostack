4de8bbf585660c81a16dccb5a10c26301a3662ba
Velenir <velenir@gmail.com>
2018-11-14 17:06:03 +0300

    fill in ContributionReward migration

---
+++ migrations/18_SCHEMES_ContributionReward.js
@@ -0,0 +1,43 @@
+/* global artifacts */
+/* eslint no-undef: "error" */
+
+const { getGenesisProtocolData } = require('../src/helpers/genesisProtocolHelper')(artifacts)
+
+const DxContributionReward = artifacts.require('DxContributionReward')
+const DxAvatar = artifacts.require('DxAvatar')
+const DxController = artifacts.require('DxController')
+
+const registerScheme = require('./helpers/registerScheme')
+
+module.exports = async function (deployer) {
+  const dxAvatar = await DxAvatar.deployed()
+  const dxController = await DxController.deployed()
+
+  console.log('Deploy DxContributionReward that inherits from ContributionReward')
+  const dxContributionReward = await deployer.deploy(DxContributionReward)
+
+  console.log('Configure DxContributionReward')
+
+  const {
+    genesisProtocolParamsHash,
+    genesisProtocolAddress
+  } = await getGenesisProtocolData()
+
+  const contributionRewardParams = [
+    0, // a fee (in the organization's token) that is to be paid for submitting a contribution
+    genesisProtocolParamsHash,
+    genesisProtocolAddress
+  ]
+
+  await dxContributionReward.setParameters(...contributionRewardParams)
+
+  const paramsHash = await dxContributionReward.getParametersHash(...contributionRewardParams)
+
+  await registerScheme({
+    label: 'DxContributionReward',
+    paramsHash,
+    schemeAddress: dxContributionReward.address,
+    avatarAddress: dxAvatar.address,
+    controller: dxController
+  })
+}
