e04193637ef85de75b13e817d92213ebf0d1c0ff
Velenir <velenir@gmail.com>
2018-11-14 15:12:27 +0300

    fill in GenericScheme migration

--- migrations/13_SCHEMES_GenericScheme.js
+++ migrations/13_SCHEMES_GenericScheme.js
@@ -1,12 +1,48 @@
 /* global artifacts */
 /* eslint no-undef: "error" */
 
-module.exports = async function (deployer) {
-  console.log('TODO: Deploy DxSchemeRegistrar that inherits from SchemeRegistrar')
-  console.log('Configure SchemeRegistrar')
-  // TODO: Check permissions OLD_dao_migration.js
-  console.log('controller.registerScheme(_schemes[i], _params[i], _permissions[i],address(_avatar));')
+const { getGenesisProtocolData } = require('../src/helpers/genesisProtocolHelper')(artifacts)
 
-  // await schemeRegistrarInst.setParameters(genesisProtocolParamsHash, genesisProtocolParamsHash, genesisProtocolInstance.address, options)
-  // var schemeRegisterParams = await schemeRegistrarInst.getParametersHash(genesisProtocolParamsHash, genesisProtocolParamsHash, genesisProtocolInstance.address, options)
+const DxGenericScheme = artifacts.require('DxGenericScheme')
+const DxAvatar = artifacts.require('DxAvatar')
+const DxController = artifacts.require('DxController')
+
+const registerScheme = require('./helpers/registerScheme')
+
+module.exports = async function (deployer) {
+  const dxAvatar = await DxAvatar.deployed()
+  const dxController = await DxController.deployed()
+
+  console.log('Deploy DxGenericScheme that inherits from GenericScheme')
+  const dxGenericScheme = await deployer.deploy(DxGenericScheme)
+
+  console.log('Configure DxGenericScheme')
+
+  const {
+    genesisProtocolParamsHash,
+    genesisProtocolAddress
+  } = await getGenesisProtocolData()
+
+  // TODO: get locally deployed Dx address
+  const dutchXContractAddress = '0x039fb002d21c1c5eeb400612aef3d64d49eb0d94'
+
+  const genericSchemeParams = [
+    // QUESTION: is it ok to just pass in hash, it expects bytes32, so a string works, but...
+    genesisProtocolParamsHash,
+    genesisProtocolAddress,
+    dutchXContractAddress
+  ]
+
+  await dxGenericScheme.setParameters(...genericSchemeParams)
+
+  const paramsHash = await dxGenericScheme.getParametersHash(...genericSchemeParams)
+
+  await registerScheme({
+    label: 'DxGenericScheme',
+    paramsHash,
+    permissions: '0x00000010',
+    schemeAddress: dxGenericScheme.address,
+    avatarAddress: dxAvatar.address,
+    controller: dxController
+  })
 }
