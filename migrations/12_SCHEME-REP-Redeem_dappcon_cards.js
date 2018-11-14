dd330d4609f913d21d242b4a10bed3233d862d5c
Velenir <velenir@gmail.com>
2018-11-14 15:10:13 +0300

    fill in RedeemDappconCards migration

--- migrations/12_SCHEME-REP-Redeem_dappcon_cards.js
+++ migrations/12_SCHEME-REP-Redeem_dappcon_cards.js
@@ -1,15 +1,47 @@
-/* global artifacts */
+/* global artifacts, web3 */
 /* eslint no-undef: "error" */
 
-module.exports = async function (deployer) {
-  console.log('TODO: Deploy RedeemDappconCards that inherits from FixedReputationAllocation')
-  console.log('Configure RedeemDappconCards')
-  // TODO: Check permissions OLD_dao_migration.js
-  console.log('controller.registerScheme(_schemes[i], _params[i], _permissions[i],address(_avatar));')
+const DxRedeemDappconCards = artifacts.require('DxRedeemDappconCards')
+const DxAvatar = artifacts.require('DxAvatar')
+const DxController = artifacts.require('DxController')
 
-  // await deployer.deploy(FixedReputationAllocation,
-  //   AvatarInst.address,
-  //   fixedReputationAllocationParamsJson.reputationReward,
-  //   options)
-  // var fixedReputationAllocationInst = await FixedReputationAllocation.deployed()
+const dateUtil = require('../src/helpers/dateUtil')
+
+const registerScheme = require('./helpers/registerScheme')
+
+const {
+  reputationReward,
+  redeemEnableTime: redeemTime
+} = require('../src/config/schemes/old/fixreputationallocationparams.json')
+
+module.exports = async function (deployer) {
+  const dxAvatar = await DxAvatar.deployed()
+  const dxController = await DxController.deployed()
+
+  console.log('Deploy DxRedeemDappconCards that inherits from FixedReputationAllocation')
+  const dxRedeemDappconCards = await deployer.deploy(DxRedeemDappconCards)
+
+  console.log('Configure DxRedeemDappconCards')
+
+  // TODO: have real times in config, for now current time + from config
+  // to ensure times are in the future
+  const redeemEnableTime = new Date(Date.now() + redeemTime * 1000)
+
+  console.log('  - Avatar address:', dxAvatar.address)
+  console.log('  - Reputation reward:', reputationReward)
+  console.log('  - Redeem enable time:', dateUtil.formatDateTime(redeemEnableTime))
+
+  await dxRedeemDappconCards.initialize(
+    dxAvatar.address,
+    reputationReward,
+    dateUtil.toEthereumTimestamp(redeemEnableTime)
+  )
+
+  await registerScheme({
+    label: 'DxRedeemDappconCards',
+    schemeAddress: dxRedeemDappconCards.address,
+    avatarAddress: dxAvatar.address,
+    controller: dxController,
+    web3
+  })
 }
