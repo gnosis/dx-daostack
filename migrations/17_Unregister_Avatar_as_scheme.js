9403e2cb8589cf37ecba53c29ab944b860fcdb41
Dmitry <Velenir@users.noreply.github.com>
2018-11-19 17:50:13 +0300

    fill in Unregister-Avatar migration (#17)

--- migrations/17_Unregister_Avatar_as_scheme.js
+++ migrations/17_Unregister_Avatar_as_scheme.js
@@ -1,11 +1,17 @@
 /* global artifacts */
 /* eslint no-undef: "error" */
 
+const DxController = artifacts.require('DxController')
+const DxAvatar = artifacts.require('DxAvatar')
+
 module.exports = async function (deployer) {
+  const dxController = await DxController.deployed()
+  const dxAvatar = await DxAvatar.deployed()
+
   console.log('Unregister avatar as a controller scheme')
   console.log('TODO: Not sure if it is needed')
 
   // Copied from daoCreator, not sure if it's required
   // Unregister self:
-  // controller.unregisterScheme(this,address(_avatar));
+  await dxController.unregisterScheme(dxController.address, dxAvatar.address)
 }
