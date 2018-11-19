b84eed3f5296c5ab85c66f5a0f3ccae6da551f9a
Dmitry <Velenir@users.noreply.github.com>
2018-11-19 17:49:48 +0300

    Migration/18 contribution reward (#18)
    * add ContributionReward contract
    
    * fill in ContributionReward migration
    
    * use submissionFee value from config

---
+++ contracts/DxContributionReward.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.24;
+
+import "@daostack/arc/contracts/universalSchemes/ContributionReward.sol";
+
+/**
+ * @title A scheme for proposing and rewarding contributions to an organization
+ */
+contract DxContributionReward is ContributionReward {
+    constructor () public {}
+}
