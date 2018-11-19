c6861553cc9a6481ad5b2f2f2bb9884eff2db958
Dmitry <Velenir@users.noreply.github.com>
2018-11-19 18:34:41 +0300

    Migration/14-SchemeRegistrar (#14)
    * fill in SchemeRegistrar migration
    
    * fix variable name

--- migrations/14_SCHEMES_SchemeRegistrar.js
+++ migrations/14_SCHEMES_SchemeRegistrar.js
@@ -1,11 +1,43 @@
 /* global artifacts */
 /* eslint no-undef: "error" */
 
-module.exports = async function (deployer) {
-  console.log('Configure GlobalConstraintRegistrar')
-  // TODO: Check permissions OLD_dao_migration.js
-  console.log('controller.registerScheme(_schemes[i], _params[i], _permissions[i],address(_avatar));')
+const { getGenesisProtocolData } = require('../src/helpers/genesisProtocolHelper')(artifacts)
 
-  // await globalConstraintRegistrarInst.setParameters(genesisProtocolParamsHash, genesisProtocolInstance.address, options)
-  // var schemeGCRegisterParams = await globalConstraintRegistrarInst.getParametersHash(genesisProtocolParamsHash, genesisProtocolInstance.address, options)
+const SchemeRegistrar = artifacts.require('SchemeRegistrar')
+const DxAvatar = artifacts.require('DxAvatar')
+const DxController = artifacts.require('DxController')
+
+const registerScheme = require('./helpers/registerScheme')
+
+module.exports = async function (deployer) {
+  const dxAvatar = await DxAvatar.deployed()
+  const dxController = await DxController.deployed()
+
+  const schemeRegistrar = await deployer.deploy(SchemeRegistrar)
+
+  console.log('Configure SchemeRegistrar')
+
+  const {
+    genesisProtocolParamsHash,
+    genesisProtocolAddress
+  } = await getGenesisProtocolData()
+
+  const schemeRegistrarParams = [
+    genesisProtocolParamsHash,
+    genesisProtocolParamsHash,
+    genesisProtocolAddress
+  ]
+
+  await schemeRegistrar.setParameters(...schemeRegistrarParams)
+
+  const paramsHash = await schemeRegistrar.getParametersHash(...schemeRegistrarParams)
+
+  await registerScheme({
+    label: 'SchemeRegistrar',
+    paramsHash,
+    permissions: '0x0000001F',
+    schemeAddress: schemeRegistrar.address,
+    avatarAddress: dxAvatar.address,
+    controller: dxController
+  })
 }
