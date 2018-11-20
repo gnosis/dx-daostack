052020cd1a850be0a54200ac7200d9a0506e0a78
Velenir <velenir@gmail.com>
2018-11-20 13:55:52 +0300

    change permissions to individual flags

--- migrations/helpers/SchemePermissions.js
+++ migrations/helpers/SchemePermissions.js
@@ -1,17 +1,31 @@
-const Default = '0x00000001'
+// All 0: Not registered,
+// 1st bit: Flag if the scheme is registered,
+// 2nd bit: Scheme can register other schemes
+// 3th bit: Scheme can add/remove global constraints
+// 4rd bit: Scheme can upgrade the controller
+// 5th bit: Scheme can call delegatecall
 
-module.exports = {
-  SchemeRegistrar: '0x0000001F',
-  GlobalConstraintRegistrar: '0x00000005',
-  UpgradeScheme: '0x0000000a',
-  // redundancy for safety
-  ContributionReward: Default,
-  GenesisProtocol: Default,
-  ExternalLocking4Reputation: Default,
-  Auction4Reputation: Default,
-  LockingEth4Reputation: Default,
-  LockingToken4Reputation: Default,
-  FixedReputationAllocation: Default,
-  GenericScheme: '0x00000010',
-  Default: Default
+const PermissionFlags = {
+  NOT_REGISTERED: 0b0,
+  REGISTERED: 0b1,
+  REGISTER_SCHEMES: 0b10,
+  ADD_REMOVE_GLOBAL_CONSTRAINTS: 0b100,
+  UPGRADE_CONTROLLER: 0b1000,
+  CALL_DELEGATECALL: 0b10000
 }
+
+// Per Scheme:
+// SchemeRegistrar: REGISTERED | REGISTER_SCHEMES | ADD_REMOVE_GLOBAL_CONSTRAINTS |
+//   UPGRADE_CONTROLLER | CALL_DELEGATECALL = '0x0000001F',
+// GlobalConstraintRegistrar: REGISTERED | ADD_REMOVE_GLOBAL_CONSTRAINTS = '0x00000005',
+// UpgradeScheme: REGISTER_SCHEMES | UPGRADE_CONTROLLER = '0x0000000a',
+// ContributionReward: REGISTERED = '0x00000001',
+// GenesisProtocol: REGISTERED = '0x00000001',
+// ExternalLocking4Reputation: REGISTERED = '0x00000001',
+// Auction4Reputation: REGISTERED = '0x00000001',
+// LockingEth4Reputation: REGISTERED = '0x00000001',
+// LockingToken4Reputation: REGISTERED = '0x00000001',
+// FixedReputationAllocation: REGISTERED = '0x00000001',
+// GenericScheme: CALL_DELEGATECALL = '0x00000010'
+
+module.exports = PermissionFlags
