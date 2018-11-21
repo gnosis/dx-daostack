0388a1db99b8edfe2fb083c87aee2ffd53b421b8
Velenir <velenir@gmail.com>
2018-11-21 16:52:22 +0300

    use same contract names for Token contracts

--- test/contracts/GnoToken.sol
+++ test/contracts/GnoToken.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.24;
 
 import "./TestToken.sol";
 
-contract GnoToken is TestToken {
+contract TokenGNO is TestToken {
     constructor (uint256 initialBalance) public
     TestToken ("Gnosis", "GNO", initialBalance) {}
 }
