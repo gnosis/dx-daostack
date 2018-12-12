fd0bc6844789ca1fa9116e696d1cda93af155ce2
Angel Rodriguez <anxolin@gmail.com>
2018-12-12 11:41:13 +0100

    Delete unused tokens

--- contracts/LocalDevelopment.sol
+++ contracts/LocalDevelopment.sol
@@ -12,10 +12,6 @@ import "@daostack/arc/contracts/universalSchemes/GenericScheme.sol";
 import "@daostack/infra/contracts/votingMachines/GenesisProtocol.sol";
 
 // Tokens
-//  First aproach, maybe later we take it from DutchX contracts
 import "../test/contracts/GenToken.sol";
-// import "../test/contracts/MgnToken.sol";
-// import "../test/contracts/EthToken.sol";
-// import "../test/contracts/GnoToken.sol";
 
 contract LocalDevelopment {}
