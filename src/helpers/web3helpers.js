6a5e23a575b71671bf6c568871b772aa86572737
Dmitry <Velenir@users.noreply.github.com>
2018-12-28 12:10:28 +0300

    Test/lock ETH (#37)
    * add bail out for increaseTime by <= 0
    
    * add test fro lock ETH
    
    * update dependencies

--- src/helpers/web3helpers.js
+++ src/helpers/web3helpers.js
@@ -1,43 +1,51 @@
 module.exports = web3 => {
-  function mineBlock () {
+  function mineBlock() {
     return web3Send({
       method: 'evm_mine'
     })
   }
 
-  function increaseTime (num) {
+  function increaseTime(num) {
+    if (num <= 0) {
+      console.log('No need to increase time')
+      return
+    }
     return web3Send({
       method: 'evm_increaseTime', params: [num]
     })
   }
 
-  async function increaseTimeAndMine (num) {
+  async function increaseTimeAndMine(num) {
+    if (num <= 0) {
+      console.log('No need to increase time')
+      return
+    }
     return [
       await increaseTime(num),
       await mineBlock()
     ]
   }
 
-  async function takeSnapshot () {
+  async function takeSnapshot() {
     return (await web3Send({
       method: 'evm_snapshot'
     })).result
   }
 
-  function revertSnapshot (id) {
+  function revertSnapshot(id) {
     return web3Send({
       method: 'evm_revert',
       params: [id]
     })
   }
 
-  function web3Send (options) {
+  function web3Send(options) {
     return new Promise((resolve, reject) => web3.currentProvider.send(
       options, (err, res) => err ? reject(err) : resolve(res))
     )
   }
 
-  async function getTimestamp (block = 'latest') {
+  async function getTimestamp(block = 'latest') {
     return (await web3.eth.getBlock(block)).timestamp
   }
 
