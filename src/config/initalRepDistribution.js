615c80fb14211cd9ef80036af1844ad29870a6bc
Velenir <velenir@gmail.com>
2019-01-22 14:29:55 +0300

    convert reward into wei + a check

--- src/config/initalRepDistribution.js
+++ src/config/initalRepDistribution.js
@@ -29,14 +29,17 @@ const totalAssignedRep =
 const assert = require('assert')
 assert(totalAssignedRep === 100, 'The percecentages must sum 100%')
 
-module.exports = {
-  whitelistedTokensReward: getReward(WHITELISTED_TOKENS_PERCENTAGE),
-  ethReward: getReward(ETH_PERCENTAGE),
-  contributorsReward: getReward(CONTRIBUTORS_PERCENTAGE),
-  genReward: getReward(GEN_PERCENTAGE),
-  mgnReward: getReward(MGN_PERCENTAGE)
-}
+module.exports = web3 => ({
+  whitelistedTokensReward: getReward(WHITELISTED_TOKENS_PERCENTAGE, web3),
+  ethReward: getReward(ETH_PERCENTAGE, web3),
+  contributorsReward: getReward(CONTRIBUTORS_PERCENTAGE, web3),
+  genReward: getReward(GEN_PERCENTAGE, web3),
+  mgnReward: getReward(MGN_PERCENTAGE, web3)
+})
 
-function getReward (percentage) {
-  return Math.ceil((INITIAL_REP_DISTRIBUTION * percentage) / 100)
+function getReward (percentage, web3) {
+  const val = Math.ceil((INITIAL_REP_DISTRIBUTION * percentage) / 100).toString()
+  // (numbers >= 1e21).toString() result in strings in E-notation, which breaks toWei()
+  assert(!val.includes('e'), 'reputation reward shouldn\'t be in E-notation')
+  return web3.utils.toWei(val)
 }
