3ad6ab7b497c4dac1aae9a5b42b0c1d8c599273f
Dmitry <Velenir@users.noreply.github.com>
2019-02-03 12:21:32 +0300

    Uncomment code that was actually used

--- migrations/15_Configure_Oracle_and_set_Avatar_as_DX_auctioneer.js
+++ migrations/15_Configure_Oracle_and_set_Avatar_as_DX_auctioneer.js
@@ -55,16 +55,16 @@ module.exports = async function (deployer, network) { // eslint-disable-line no-
     }
   }
 
-  // if (process.env.USE_MOCK_DX) {
-  //   const Wallet = artifacts.require('Wallet')
-  //   const WalletDeployed = await Wallet.deployed()
+  if (process.env.USE_MOCK_DX) {
+    const Wallet = artifacts.require('Wallet')
+    const WalletDeployed = await Wallet.deployed()
 
-  //   const DxAvatar = artifacts.require('DxAvatar')
-  //   const dxAvatar = await DxAvatar.deployed()
+    const DxAvatar = artifacts.require('DxAvatar')
+    const dxAvatar = await DxAvatar.deployed()
 
-  //   console.log('Setting Avatar at address', dxAvatar.address, 'as Wallet\'s owner');
-  //   await WalletDeployed.transferOwnership(dxAvatar.address)
+    console.log('Setting Avatar at address', dxAvatar.address, 'as Wallet\'s owner');
+    await WalletDeployed.transferOwnership(dxAvatar.address)
 
-  //   console.log('Wallet\'s owner is now:', await WalletDeployed.owner());
-  // }
+    console.log('Wallet\'s owner is now:', await WalletDeployed.owner());
+  }
 }
