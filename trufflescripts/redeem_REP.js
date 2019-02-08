d502c8de666297abe456ba0b00496030e21b3468
David <david.sato64@gmail.com>
2019-02-08 11:02:03 +0200

    redeem rep pt 2

--- trufflescripts/redeem_REP.js
+++ trufflescripts/redeem_REP.js
@@ -11,6 +11,9 @@ const DxLockWhitelisted4RepArtifact = artifacts.require('DxLockWhitelisted4Rep')
 // contracts in ./networks-3rd-rinkeby-test.json should have those
 // then call Contract.redeem(account)
 
+// To help console testing
+// const dxLMR = await DxLockMgnForRep.at('0x6099974d7Ed074110db69C515EC748893df43f13'); const dxLER = await DxLockEth4Rep.at('0x311814CAfb902C72e87aAbC2978751B7314646e6'); const dxLWR = await DxLockWhitelisted4Rep.at('0x1f05d55Cf3FA74eA658D87E48c60C5199Bad4caF'); const dxBAG = await DxGenAuction4Rep.at('0x2B19c60d6934E2f20515a8aECCaC4a5c58221BD4')
+
 module.exports = async () => {
 
   const argv = require('yargs')
@@ -77,11 +80,15 @@ module.exports = async () => {
       ]))
     }
     
+    // 1. Check all Lock events for dxLockMGNForRep, dxLockETHForRep, and dxLockWhitelistForRep
+    //TODO: should this be 1 array? Does it matter if it's split?
     const [dxLMR_LOCK_EVENTS, dxLER_LOCK_EVENTS, dxLWR_LOCK_EVENTS] = await Promise.all([
       dxLMR.getPastEvents('Lock', { fromBlock: 0 }),
       dxLER.getPastEvents('Lock', { fromBlock: 0 }),
       dxLWR.getPastEvents('Lock', { fromBlock: 0 }),
     ])
+
+    // 2. Of all the beneficiaries in step 1, use only the ones that ALSO have
     console.log('TCL: dxLMR_LOCK_EVENTS, dxLER_LOCK_EVENTS, dxLWR_LOCK_EVENTS', dxLMR_LOCK_EVENTS, dxLER_LOCK_EVENTS, dxLWR_LOCK_EVENTS) 
   } catch (error) {
     console.error(error)
