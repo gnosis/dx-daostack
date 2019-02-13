e20e06e442bd4c6adabc724d716aedd83609e685
Angel Rodriguez <anxolin@gmail.com>
2019-02-13 17:21:34 +0100

    Improve logging output

--- migrations/18_HELPER_DxLockClaimRedeemHelper.js
+++ migrations/18_HELPER_DxLockClaimRedeemHelper.js
@@ -1,36 +1,37 @@
-/* // Deploy DxLockMgnForRepHelper
-console.log('Deploying DxLockMgnForRepHelper helper')
-console.log('  - Helper that allows to batch claim all MGN')
-await deployer.deploy(DxDaoClaimRedeemHelper, dxLockMgn.address) */
-
-/* global artifacts, web3 */
+/* global artifacts, Promise */
 /* eslint no-undef: "error" */
-const assert = require('assert')
-const dateUtil = require('../src/helpers/dateUtil')
-const { registerScheme } = require('./helpers/schemeUtils')
-
-const {
-    getLockedMgnSignature
-} = require('../src/config/bootstrap')
-
-// TODO: Once we use the latest contracts, we should use all this dates.
 
 module.exports = async function (deployer) {
-    const DxLockMgnForRep = artifacts.require('DxLockMgnForRep')
-    const DxLockEth4Rep = artifacts.require('DxLockEth4Rep')
-    const DxLockWhitelisted4Rep = artifacts.require('DxLockWhitelisted4Rep')
-    const DxGenAuction4Rep = artifacts.require('DxGenAuction4Rep')
-    const DxDaoClaimRedeemHelper = artifacts.require('DxDaoClaimRedeemHelper')
+  const DxLockMgnForRep = artifacts.require('DxLockMgnForRep')
+  const DxLockEth4Rep = artifacts.require('DxLockEth4Rep')
+  const DxLockWhitelisted4Rep = artifacts.require('DxLockWhitelisted4Rep')
+  const DxGenAuction4Rep = artifacts.require('DxGenAuction4Rep')
+  const DxDaoClaimRedeemHelper = artifacts.require('DxDaoClaimRedeemHelper')
 
-    // Get DxLockMgnForRep
-    const [dxLMR, dxLER, dxLWR, dxGAR] = await Promise.all([
-        DxLockMgnForRep.deployed(),
-        DxLockEth4Rep.deployed(),
-        DxLockWhitelisted4Rep.deployed(),
-        DxGenAuction4Rep.deployed(),
-    ])
+  // Get the locking contracts
+  const [
+    dxLockMgnForRep,
+    dxLockEth4Rep,
+    dxLockWhitelisted4Rep,
+    dxGenAuction4Rep
+  ] = await Promise.all([
+    DxLockMgnForRep.deployed(),
+    DxLockEth4Rep.deployed(),
+    DxLockWhitelisted4Rep.deployed(),
+    DxGenAuction4Rep.deployed(),
+  ])
 
-    console.log('TCL: dxLMR, dxLER, dxLWR, dxGAR', dxLMR.address, dxLER.address, dxLWR.address, dxGAR.address)
-
-    return deployer.deploy(DxDaoClaimRedeemHelper, dxLER.address, dxLMR.address, dxLWR.address, dxGAR.address)
+  // Deploy DxLockMgnForRepHelper
+  console.log('Deploying DxDaoClaimRedeemHelper helper: Allows to batch claim all MGN')
+  console.log('  - DxLockMgnForRep address: ' + dxLockMgnForRep.address)
+  console.log('  - DxLockEth4Rep address: ' + dxLockEth4Rep.address)
+  console.log('  - DxLockWhitelisted4Rep', dxLockWhitelisted4Rep.address)
+  console.log('  - DxGenAuction4Rep', dxGenAuction4Rep.address)
+  return deployer.deploy(
+    DxDaoClaimRedeemHelper,
+    dxLockEth4Rep.address,
+    dxLockMgnForRep.address,
+    dxLockWhitelisted4Rep.address,
+    dxGenAuction4Rep.address
+  )
 }
