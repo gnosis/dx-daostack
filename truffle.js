b86a19f480e7d537a5104b94daa65811fc929545
Velenir <velenir@gmail.com>
2019-02-21 13:50:46 +0300

    update compiler version to 0.5.4

--- truffle.js
+++ truffle.js
@@ -124,7 +124,7 @@ function truffleConfig({
     },
     compilers: {
       solc: {
-        version: '0.5.2',
+        version: '0.5.4',
         docker: process.env.SOLC_USE_DOCKER === 'true' || false,
         settings: {
           optimizer: {
