1f885de398d73e1b57d6104c169ce4e09e6d76bc
Angel Rodriguez <anxolin@gmail.com>
2019-04-16 16:31:27 +0200

    Allow to create proposals using ContributionReward

--- test/resources/proposals/test-proposals.js
+++ test/resources/proposals/test-proposals.js
@@ -1,7 +1,7 @@
 module.exports = [
-  // updateThresholdNewAuction(uint _thresholdNewAuction)
   {
-    type: 'CHANGE-THRESHOLD-NEW-AUCTION',
-    newThresholdInUsd: 2000
+    type: 'SEND-ETHER',
+    toAddress: '0x3d1df1a816577a62db61281f673c4f43ae063490',
+    amount: 1e15  // 0.001 in Wei
   }
 ]
