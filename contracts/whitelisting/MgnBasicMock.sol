2bfd128ae8b0bf978d598898e0bac728306d5f0d
Velenir <velenir@gmail.com>
2019-05-08 17:40:36 +0300

    add .lockMultiple to MGN Mocks

--- contracts/whitelisting/MgnBasicMock.sol
+++ contracts/whitelisting/MgnBasicMock.sol
@@ -6,7 +6,13 @@ contract MgnBasicMock {
     // user => amount
     mapping (address => uint) public lockedTokenBalances;
 
-    function lock(uint256 _amount) public {
+    function lock(uint256 _amount) external {
         lockedTokenBalances[msg.sender] = _amount;
+    }
+
+    function lockMultiple(uint256 _amount, address[] calldata _beneficiaries) external {
+        for (uint i = 0; i < _beneficiaries.length; ++i) {
+            lockedTokenBalances[_beneficiaries[i]] = _amount;
+        }
     }
 }
--- contracts/whitelisting/MgnOwnableMock.sol
+++ contracts/whitelisting/MgnOwnableMock.sol
@@ -2,12 +2,19 @@ pragma solidity ^0.5.4;
 
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
+
 contract MgnOwnableMock is Ownable {
 
     // user => amount
     mapping (address => uint) public lockedTokenBalances;
 
-    function lock(uint256 _amount, address _beneficiary) public onlyOwner {
+    function lock(uint256 _amount, address _beneficiary) external onlyOwner {
         lockedTokenBalances[_beneficiary] = _amount;
+    }
+
+    function lockMultiple(uint256 _amount, address[] calldata _beneficiaries) external onlyOwner {
+        for (uint i = 0; i < _beneficiaries.length; ++i) {
+            lockedTokenBalances[_beneficiaries[i]] = _amount;
+        }
     }
 }
