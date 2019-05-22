6e05fa6091aa0c65bb7ec883008c7b7d7ac1afe8
Velenir <velenir@gmail.com>
2019-05-22 16:09:01 +0300

    better getPastEvents func that does blockSizing

--- src/scripts/utils/index.js
+++ src/scripts/utils/index.js
@@ -6,9 +6,94 @@ module.exports = (web3) => {
         return (await web3.eth.getBlock(block)).timestamp
     }
 
+    async function retry(cb, attempt = 1, maxAttempts = 10) {
+        try {
+          return await cb()
+        } catch (e) {
+          // const waitTime = attempt * attempt * WAIT_TIME
+          // console.error(`\nError claiming MGN. Retrying in ${waitTime / 1000} seconds. ${maxAttempts - attempt} remaining attempts\n`)
+          console.error(`\nError redeeming. Retrying. ${maxAttempts - attempt} remaining attempts\n`)
+      
+          if (attempt >= maxAttempts) {
+            console.log('Out of attempts')
+            throw e
+          } else {
+            // await wait(waitTime)
+            console.warn(e.message)
+            return await retry(cb, attempt + 1, maxAttempts)
+          }
+        }
+      }
+
+    function parseEventLog({returnValues}) {
+        return Object.entries(returnValues).reduce((accum, [k,v]) => {
+          if(Number.isNaN(+k))accum[k]=v
+          return accum
+          },{})
+      }
+
+    async function getPastEvents(contract, eventName, { fromBlock = 0, toBlock, blockBatchSize = 30, ...rest} = {}) {
+        if (!toBlock || toBlock === 'latest') ({number: toBlock} = await web3.eth.getBlock('latest'));
+  
+        try {
+          console.log(`Fetching event ${eventName} from contract ${contract.constructor.contractName}`);
+          const results = await contract.getPastEvents(eventName, { fromBlock, toBlock, ...rest})
+          console.log('single batch: ', results.map(parseEventLog));
+          return results
+        } catch (error) {
+          if (error.message.includes('query returned more than 1000 results')) {
+            console.warn(error.message)
+            console.log(`Will try getting events from ${blockBatchSize} blocks at a time`)
+  
+            const results = []
+  
+            let gotEvents = 0
+  
+            for (let i = fromBlock; i <= toBlock; i += blockBatchSize) {
+              const toBlockBatch = Math.min(i + blockBatchSize - 1, toBlock)
+              console.log(`\n  [Fetch event ${eventName} from block ${i} to ${toBlockBatch} from contract ${contract.constructor.contractName}]`)
+              const batch = await retry(() => {
+                return contract.getPastEvents(eventName, { fromBlock: i, toBlock: toBlockBatch, ...rest})
+              })
+              console.log('batch: ', batch.map(parseEventLog));
+  
+              results.push(...batch)
+              gotEvents += batch.length
+              if (gotEvents > 1000) {
+                gotEvents = 0
+                console.log('Got more than 1000 events. Trying fetching the rest in one bunch.');
+                console.log(`\n  [Fetch event ${eventName} from block ${i + blockBatchSize} to ${toBlock} from contract ${contract.constructor.contractName}]`)
+                try {
+                  const batch = await retry(() => {
+                    return contract.getPastEvents(eventName, { fromBlock: i + blockBatchSize, toBlock, ...rest})
+                  })
+                  console.log('batch: ', batch.map(parseEventLog));
+      
+                  results.push(...batch)
+                  return results
+                } catch (error) {
+                  if (error.message.includes('query returned more than 1000 results')) {
+                    console.warn(error.message)
+                    console.log(`Continuing getting events from ${blockBatchSize} blocks at a time`)
+                  }
+                }
+              }
+              // await wait(WAIT_TIME)
+            }
+  
+            return results
+          }
+  
+          throw error
+        }
+      }
+
     return {
         toBN,
         mapToString,
-        getTimestamp
+        getTimestamp,
+        parseEventLog,
+        retry,
+        getPastEvents
     }
 }
